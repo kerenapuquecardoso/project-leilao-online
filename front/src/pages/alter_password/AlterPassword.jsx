cd52a0fe2af2dbcf272fd881c6b0dcd7d5847ae7
Keren Apuque <cardosokerenapuque@gmail.com>
2024-08-20 12:15:59 -0300

    refactor: implementada parcialmente a funcionalidade de comparar as senhas

--- front/src/pages/alter_password/AlterPassword.jsx
+++ front/src/pages/alter_password/AlterPassword.jsx
@@ -4,26 +4,43 @@ import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
-import { ConfirmDialog } from 'primereact/confirmdialog'; // For <ConfirmDialog /> component
-import { confirmDialog } from 'primereact/confirmdialog'; // For confirmDialog method
-        
-                      
-        
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ValidationPassword from "../../components/validation-password/ValidationPassword";
+
+
 const AlterPassword = () => {
+    const navigate = useNavigate();
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordsMatch, setPasswordsMatch] = useState("");
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const confirmPasswordInput = (e) => {
+        setConfirmPassword(e.target.value);
+        if(password !== confirmPassword){
+            setPasswordsMatch("As senhas não conferem");
+        }else{
+            setPasswordsMatch("");
+        }
+    };
+
+    
     return(
         <div className="alter-password-body">
-            <Card title="AlterPassword" className="flex flex-columns justify-content-center md:w-30rem align-items-center text-center mt-5">
+            <Card title="Alterar Senha" className="flex flex-columns justify-content-center md:w-30rem align-items-center text-center mt-5">
                 <InputText className="align-items-center justify-content-center mt-3" placeholder="email" />
                 <InputText className="align-items-center justify-content-center mt-3" placeholder="código" />
-                <Password feedback={false} className="flex align-items-center justify-content-center mt-3"placeholder="senha"/>
-                <Password feedback={false} className="flex align-items-center justify-content-center mt-3"placeholder="confirmar senha"/>
-                <Button  className="align-items-center justify-content-center mt-3" label="Alterar senha"/><br/>
-                <Button label="cancelar" className="flex-row  justify-content-center align-content-between mt-5" text/>
+                <ValidationPassword onChange={handlePasswordChange}/>
+                <Password onChange={confirmPasswordInput} feedback={false} className="flex align-items-center justify-content-center mt-3"placeholder="confirmar senha"/>
+                {passwordsMatch && <small>{passwordsMatch}</small>}
+                <div className="btn-alter-password"> <Button className="align-items-center justify-content-center mt-3" label="Alterar senha"onClick={() => navigate('/', '_blank')}  /></div>
+                <Button label="cancelar"className="flex-row  justify-content-center align-content-between mt-5" link onClick={() => navigate('/login', '_blank' )}/>
             </Card>
-            
-
         </div>
-        
     );
 }
 
