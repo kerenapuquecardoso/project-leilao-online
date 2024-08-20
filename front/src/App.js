5818c20313be5f1993b3fdf1cd6aae46135e69dd
Keren Apuque <cardosokerenapuque@gmail.com>
2024-08-20 12:14:13 -0300

    feat: implementado o compoente que valida os caracetres da senha

--- front/src/App.js
+++ front/src/App.js
@@ -7,18 +7,20 @@ import './App.css';
 import DefaultLayout from './components/DefaultLayout';
 import ResetPassword from './pages/reset_password/ResetPassword';
 import AlterPassword from './pages/alter_password/AlterPassword'; 
+import PrivateRouter from './components/PrivateRouter';
 
 function App() {
   return (
     <>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<DefaultLayout><Home/></DefaultLayout>}/>
+            <Route element={<PrivateRouter/>}>
+              <Route path='/' element={<DefaultLayout><Home/></DefaultLayout>}/>
+            </Route>
             <Route path='/login' element={<SimpleLayout><Login/></SimpleLayout>}/>
             <Route path='/login/submit' element={<SimpleLayout><Submit/></SimpleLayout>}/>
             <Route path='/login/reset-password' element={<SimpleLayout><ResetPassword/></SimpleLayout>}/>
-            <Route path='/login/alter-password' element={<SimpleLayout><AlterPassword/></SimpleLayout>}/>
-
+            <Route path='/login/reset-password/alter-password' element={<SimpleLayout><AlterPassword/></SimpleLayout>}/>
           </Routes>
         </BrowserRouter>
     </>
